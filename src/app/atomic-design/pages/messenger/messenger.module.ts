3e9bf1b02bfcffe73bdae026a5629d29f2818bd5
Samir Ajdarpasic <samir_hajdarpasic@hotmail.com>
2020-08-01 18:35:05 +0200

    added basic layout for messenger

--- src/app/atomic-design/pages/messenger/messenger.module.ts
+++ src/app/atomic-design/pages/messenger/messenger.module.ts
@@ -1,12 +1,13 @@
 import {NgModule} from '@angular/core';
 import {MessengerComponent} from './messenger.component';
 import {ChatListComponent} from './components/chat-list/chat-list.component';
+import { ChatComponent } from './components/chat/chat.component';
 
 @NgModule({
   exports: [
     MessengerComponent
   ],
-  declarations: [MessengerComponent, ChatListComponent]
+  declarations: [MessengerComponent, ChatListComponent, ChatComponent]
 })
 // NOTE: we use this module to declare the components that will be used for the messenger
 export class MessengerModule {}
